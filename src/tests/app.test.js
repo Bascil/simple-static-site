ba8541636a28a8f9995ee311b618d980ca48930c
Basil Ndonga <basilndonga@gmail.com>
2022-02-25 12:17:27 +0300

    added tests and .env

--- src/tests/app.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from '../App';
-
-test('render text in components', () => {
-  render(<App />);
-  expect(screen.getByText('Air Quality')).toBeInTheDocument();
-});
